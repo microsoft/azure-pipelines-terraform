c96036d222c3beee0d0115b1d5dc132d98dbea75
Jared Holgate <jaredholgate@microsoft.com>
2023-07-08 14:48:06 +0100

    First iteration of support for OIDC

--- Tasks/TerraformTask/TerraformTaskV4/src/azure-terraform-command-handler.ts
+++ Tasks/TerraformTask/TerraformTaskV4/src/azure-terraform-command-handler.ts
@@ -2,6 +2,8 @@ import tasks = require('azure-pipelines-task-lib/task');
 import {ToolRunner} from 'azure-pipelines-task-lib/toolrunner';
 import {TerraformAuthorizationCommandInitializer} from './terraform-commands';
 import {BaseTerraformCommandHandler} from './base-terraform-command-handler';
+import { getHandlerFromToken, WebApi } from "azure-devops-node-api";
+import { ITaskApi } from "azure-devops-node-api/TaskApi";
 
 export class TerraformCommandHandlerAzureRM extends BaseTerraformCommandHandler {
     constructor() {
@@ -10,8 +12,26 @@ export class TerraformCommandHandlerAzureRM extends BaseTerraformCommandHandler 
     }
 
     private setupBackend(backendServiceName: string) {
-        var serviceprincipalid = tasks.getEndpointAuthorizationParameter(backendServiceName, "serviceprincipalid", true);
-        var serviceprincipalkey = tasks.getEndpointAuthorizationParameter(backendServiceName, "serviceprincipalkey", true);
+        var authorizationScheme : AuthorizatonScheme = AuthorizatonScheme[tasks.getEndpointAuthorizationScheme(backendServiceName, false).toLowerCase()];
+
+        switch(authorizationScheme) {
+            case AuthorizatonScheme.ServicePrincipal:
+                var servicePrincipalCredentials : ServicePrincipalCredentials = this.getServicePrincipalCredentials(backendServiceName);
+                this.backendConfig.set('client_id', servicePrincipalCredentials.servicePrincipalId);
+                this.backendConfig.set('client_secret', servicePrincipalCredentials.servicePrincipalKey);
+                break;
+
+            case AuthorizatonScheme.ManagedServiceIdentity:
+                this.backendConfig.set('use_msi', 'true');
+                break;
+
+            case AuthorizatonScheme.WorkloadIdentityFederation:
+                var workloadIdentityFederationCredentials : WorkloadIdentityFederationCredentials = this.getWorkloadIdentityFederationCredentials(backendServiceName);
+                this.backendConfig.set('client_id', workloadIdentityFederationCredentials.servicePrincipalId);
+                this.backendConfig.set('oidc_request_token', workloadIdentityFederationCredentials.idToken);
+                this.backendConfig.set('use_oidc', 'true');
+                break;
+        }
 
         this.backendConfig.set('storage_account_name', tasks.getInput("backendAzureRmStorageAccountName", true));
         this.backendConfig.set('container_name', tasks.getInput("backendAzureRmContainerName", true));
@@ -19,13 +39,6 @@ export class TerraformCommandHandlerAzureRM extends BaseTerraformCommandHandler 
         this.backendConfig.set('resource_group_name', tasks.getInput("backendAzureRmResourceGroupName", true));
         this.backendConfig.set('subscription_id', tasks.getEndpointDataParameter(backendServiceName, "subscriptionid", true));
         this.backendConfig.set('tenant_id', tasks.getEndpointAuthorizationParameter(backendServiceName, "tenantid", true));
-        
-        if(serviceprincipalid && serviceprincipalkey) {
-            this.backendConfig.set('client_id', serviceprincipalid);
-            this.backendConfig.set('client_secret', serviceprincipalkey);
-        } else {
-            this.backendConfig.set('use_msi', 'true');
-        }
     }
 
     public handleBackend(terraformToolRunner: ToolRunner): void {
@@ -39,18 +52,93 @@ export class TerraformCommandHandlerAzureRM extends BaseTerraformCommandHandler 
 
     public handleProvider(command: TerraformAuthorizationCommandInitializer) {
         if (command.serviceProvidername) {
-            var serviceprincipalid = tasks.getEndpointAuthorizationParameter(command.serviceProvidername, "serviceprincipalid", true);
-            var serviceprincipalkey = tasks.getEndpointAuthorizationParameter(command.serviceProvidername, "serviceprincipalkey", true);
+            var authorizationScheme : AuthorizatonScheme = AuthorizatonScheme[tasks.getEndpointAuthorizationScheme(command.serviceProvidername, false).toLowerCase()];
+
+            switch(authorizationScheme) {
+                case AuthorizatonScheme.ServicePrincipal:
+                    var servicePrincipalCredentials : ServicePrincipalCredentials = this.getServicePrincipalCredentials(command.serviceProvidername);
+                    process.env['ARM_CLIENT_ID'] = servicePrincipalCredentials.servicePrincipalId;
+                    process.env['ARM_CLIENT_SECRET'] = servicePrincipalCredentials.servicePrincipalKey;
+                    break;
+    
+                case AuthorizatonScheme.ManagedServiceIdentity:
+                    process.env['ARM_USE_MSI'] = 'true';
+                    break;
+    
+                case AuthorizatonScheme.WorkloadIdentityFederation:
+                    var workloadIdentityFederationCredentials : WorkloadIdentityFederationCredentials = this.getWorkloadIdentityFederationCredentials(command.serviceProvidername);
+                    process.env['ARM_CLIENT_ID'] = workloadIdentityFederationCredentials.servicePrincipalId;
+                    process.env['ARM_OIDC_TOKEN'] = workloadIdentityFederationCredentials.idToken;
+                    process.env['ARM_USE_OIDC'] = 'true';
+                    break;
+            }
 
             process.env['ARM_SUBSCRIPTION_ID']  = tasks.getEndpointDataParameter(command.serviceProvidername, "subscriptionid", false);
             process.env['ARM_TENANT_ID']        = tasks.getEndpointAuthorizationParameter(command.serviceProvidername, "tenantid", false);
-            
-            if(serviceprincipalid && serviceprincipalkey) {
-                process.env['ARM_CLIENT_ID']        = tasks.getEndpointAuthorizationParameter(command.serviceProvidername, "serviceprincipalid", true);
-                process.env['ARM_CLIENT_SECRET']    = tasks.getEndpointAuthorizationParameter(command.serviceProvidername, "serviceprincipalkey", true); 
-            } else {
-                process.env['ARM_USE_MSI'] = 'true';
-            }
         }
     }
+
+    private getServicePrincipalCredentials(connectionName: string) : ServicePrincipalCredentials {
+        var servicePrincipalCredentials : ServicePrincipalCredentials;
+        servicePrincipalCredentials.servicePrincipalId = tasks.getEndpointAuthorizationParameter(connectionName, "serviceprincipalid", true);
+        servicePrincipalCredentials.servicePrincipalKey = tasks.getEndpointAuthorizationParameter(connectionName, "serviceprincipalkey", true);
+        return servicePrincipalCredentials;
+    }
+
+    private getWorkloadIdentityFederationCredentials(connectionName: string) : WorkloadIdentityFederationCredentials {
+        var workloadIdentityFederationCredentials : WorkloadIdentityFederationCredentials;
+        workloadIdentityFederationCredentials.servicePrincipalId = tasks.getEndpointAuthorizationParameter(connectionName, "serviceprincipalid", true);
+        var connectedService: string = tasks.getInput(connectionName, true);
+        var idToken : string;
+        this.getIdToken(connectedService).then((token) => { idToken = token; });
+        workloadIdentityFederationCredentials.idToken = idToken;
+        return workloadIdentityFederationCredentials;
+    }
+
+    private async getIdToken(connectedService: string) : Promise<string> {
+        const jobId = tasks.getVariable("System.JobId");
+        const planId = tasks.getVariable("System.PlanId");
+        const projectId = tasks.getVariable("System.TeamProjectId");
+        const hub = tasks.getVariable("System.HostType");
+        const uri = tasks.getVariable("System.CollectionUri");
+        const token = this.getSystemAccessToken();
+
+        const authHandler = getHandlerFromToken(token);
+        const connection = new WebApi(uri, authHandler);
+        const api: ITaskApi = await connection.getTaskApi();
+        const response = await api.createOidcToken({}, projectId, hub, planId, jobId, connectedService);
+        if (response == null) {
+            return null;
+        }
+
+        return response.oidcToken;
+    }
+
+    private getSystemAccessToken() : string {
+        tasks.debug('Getting credentials for local feeds');
+        const auth = tasks.getEndpointAuthorization('SYSTEMVSSCONNECTION', false);
+        if (auth.scheme === 'OAuth') {
+            tasks.debug('Got auth token');
+            return auth.parameters['AccessToken'];
+        }
+        else {
+            tasks.warning('Could not determine credentials to use');
+        }
+    }
+}
+
+interface ServicePrincipalCredentials {
+    servicePrincipalId: string;
+    servicePrincipalKey: string;
+}
+
+interface WorkloadIdentityFederationCredentials {
+    servicePrincipalId: string;
+    idToken: string;
+}
+
+enum AuthorizatonScheme {
+    ServicePrincipal = "serviceprincipal",
+    ManagedServiceIdentity = "managedserviceidentity",
+    WorkloadIdentityFederation = "workloadidentityfederation"   
 }
