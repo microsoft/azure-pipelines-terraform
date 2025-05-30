01efa53bb4dbaea13eeb17a2a2d46715926dae46
Jared Holgate <jaredholgate@microsoft.com>
2025-05-30 09:56:17 +0100

    feat: add fallback to id token generation to v5 task (#300)
    * feat: add fallback to id token generation to v5 task
    
    * update task version
    
    * Update desc
    
    * typo
    
    * Add more info to the docs
    
    * remove erroneous version from description to avoid confusion
    
    * cache client id, service connection id and auth type in backedn config to enable multiple identity setup
    
    * bug fixes
    
    * fix bug with provider auth
    
    * update docs with example for multi sc
    
    * doc tweaks
    
    * complete pipeline example
    
    * docs typo

--- Tasks/TerraformTask/TerraformTaskV5/src/azure-terraform-command-handler.ts
+++ Tasks/TerraformTask/TerraformTaskV5/src/azure-terraform-command-handler.ts
@@ -3,6 +3,7 @@ import {ToolRunner} from "azure-pipelines-task-lib/toolrunner";
 import {TerraformAuthorizationCommandInitializer} from "./terraform-commands";
 import {BaseTerraformCommandHandler} from "./base-terraform-command-handler";
 import {EnvironmentVariableHelper} from "./environment-variables";
+import {generateIdToken} from './id-token-generator';
 
 export class TerraformCommandHandlerAzureRM extends BaseTerraformCommandHandler {
     constructor() {
@@ -41,7 +42,10 @@ export class TerraformCommandHandlerAzureRM extends BaseTerraformCommandHandler 
             this.backendConfig.set("use_azuread_auth", "true");
         }
 
-        this.setCommonEnvironmentVariables(authorizationScheme, serviceConnectionID);
+        let fallbackToIdTokenGeneration = tasks.getBoolInput("backendAzureRmUseIdTokenGeneration", false);
+        let backendAzureRmUseCliFlagsForAuthentication = tasks.getBoolInput("backendAzureRmUseCliFlagsForAuthentication", false);
+
+        await this.setCommonVariables(authorizationScheme, serviceConnectionID, fallbackToIdTokenGeneration, backendAzureRmUseCliFlagsForAuthentication);
 
         for (let [key, value] of this.backendConfig.entries()) {
             terraformToolRunner.arg(`-backend-config=${key}=${value}`);
@@ -65,12 +69,14 @@ export class TerraformCommandHandlerAzureRM extends BaseTerraformCommandHandler 
             EnvironmentVariableHelper.setEnvironmentVariable("ARM_SUBSCRIPTION_ID", subscriptionId);
         }
 
-        this.setCommonEnvironmentVariables(authorizationScheme, serviceConnectionID);
+        let fallbackToIdTokenGeneration = tasks.getBoolInput("environmentAzureRmUseIdTokenGeneration", false);
+
+        await this.setCommonVariables(authorizationScheme, serviceConnectionID, fallbackToIdTokenGeneration, false);
 
         tasks.debug("Finished up provider for authorization scheme: " + authorizationScheme + ".");
     }
 
-    private setCommonEnvironmentVariables(authorizationScheme: AuthorizationScheme, serviceConnectionID: string) {
+    private async setCommonVariables(authorizationScheme: AuthorizationScheme, serviceConnectionID: string, fallbackToIdTokenGeneration: boolean, useCliFlagsForBackend: boolean) : Promise<void> {
         EnvironmentVariableHelper.setEnvironmentVariable("ARM_TENANT_ID", tasks.getEndpointAuthorizationParameter(serviceConnectionID, "tenantid", false));
 
         switch(authorizationScheme) {
@@ -79,11 +85,30 @@ export class TerraformCommandHandlerAzureRM extends BaseTerraformCommandHandler 
                 break;
 
             case AuthorizationScheme.WorkloadIdentityFederation:
-                var workloadIdentityFederationCredentials = this.getWorkloadIdentityFederationCredentials(serviceConnectionID);
-                EnvironmentVariableHelper.setEnvironmentVariable("ARM_CLIENT_ID", workloadIdentityFederationCredentials.servicePrincipalId);
-                EnvironmentVariableHelper.setEnvironmentVariable("ARM_OIDC_AZURE_SERVICE_CONNECTION_ID", serviceConnectionID);
-                EnvironmentVariableHelper.setEnvironmentVariable("ARM_USE_OIDC", "true");
-                EnvironmentVariableHelper.setEnvironmentVariable("ARM_OIDC_REQUEST_TOKEN", tasks.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false));
+                var workloadIdentityFederationCredentials = await this.getWorkloadIdentityFederationCredentials(serviceConnectionID, fallbackToIdTokenGeneration);
+                if(useCliFlagsForBackend) {
+                    // By persisting the client ID in the backend config, we can support multiple service connections for backend and provider auth.
+                    this.backendConfig.set("client_id", workloadIdentityFederationCredentials.servicePrincipalId);
+                    this.backendConfig.set("use_oidc", "true");
+                } else {
+                    EnvironmentVariableHelper.setEnvironmentVariable("ARM_CLIENT_ID", workloadIdentityFederationCredentials.servicePrincipalId);
+                    EnvironmentVariableHelper.setEnvironmentVariable("ARM_USE_OIDC", "true");
+                }
+
+                if (fallbackToIdTokenGeneration) {
+                    tasks.debug("ID token generation fallback is enabled, generating ID Token.");
+                    EnvironmentVariableHelper.setEnvironmentVariable("ARM_OIDC_TOKEN", workloadIdentityFederationCredentials.oidcToken);
+                } else {
+                    tasks.debug("ID token generation fallback is disabled, using ID Token Refresh.");
+                    if(useCliFlagsForBackend) {
+                        // By persisting the service connection ID in the backend config, we can support multiple service connections for backend and provider auth.
+                        this.backendConfig.set("ado_pipeline_service_connection_id", serviceConnectionID);
+                    } else {
+                        EnvironmentVariableHelper.setEnvironmentVariable("ARM_OIDC_AZURE_SERVICE_CONNECTION_ID", serviceConnectionID);
+                    }
+                    EnvironmentVariableHelper.setEnvironmentVariable("ARM_OIDC_REQUEST_TOKEN", tasks.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false));
+                }
+
                 break;
 
             case AuthorizationScheme.ServicePrincipal:
@@ -96,17 +121,21 @@ export class TerraformCommandHandlerAzureRM extends BaseTerraformCommandHandler 
         }
     }
 
-    private getServicePrincipalCredentials(connectionName: string) : ServicePrincipalCredentials {
+    private getServicePrincipalCredentials(serviceConnectionID: string) : ServicePrincipalCredentials {
         let servicePrincipalCredentials : ServicePrincipalCredentials = {
-            servicePrincipalId: tasks.getEndpointAuthorizationParameter(connectionName, "serviceprincipalid", true),
-            servicePrincipalKey: tasks.getEndpointAuthorizationParameter(connectionName, "serviceprincipalkey", true)
+            servicePrincipalId: tasks.getEndpointAuthorizationParameter(serviceConnectionID, "serviceprincipalid", true),
+            servicePrincipalKey: tasks.getEndpointAuthorizationParameter(serviceConnectionID, "serviceprincipalkey", true)
         }
         return servicePrincipalCredentials;
     }
 
-    private getWorkloadIdentityFederationCredentials(connectionName: string) : WorkloadIdentityFederationCredentials {
+    private async getWorkloadIdentityFederationCredentials(serviceConnectionID: string, getIdToken: boolean) : Promise<WorkloadIdentityFederationCredentials> {
         let workloadIdentityFederationCredentials : WorkloadIdentityFederationCredentials = {
-            servicePrincipalId: tasks.getEndpointAuthorizationParameter(connectionName, "serviceprincipalid", true),
+            servicePrincipalId: tasks.getEndpointAuthorizationParameter(serviceConnectionID, "serviceprincipalid", true),
+            oidcToken: ""
+        }
+        if(getIdToken) {
+            workloadIdentityFederationCredentials.oidcToken = await generateIdToken(serviceConnectionID);
         }
         return workloadIdentityFederationCredentials;
     }
@@ -141,6 +170,7 @@ interface ServicePrincipalCredentials {
 
 interface WorkloadIdentityFederationCredentials {
     servicePrincipalId: string;
+    oidcToken: string;
 }
 
 enum AuthorizationScheme {
